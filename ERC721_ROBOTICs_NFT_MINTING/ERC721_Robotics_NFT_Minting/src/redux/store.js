afcb0a5c09218a93e01671fb34fcf517116d3ad5
Hasansattar <hasansattar650@gmail.com>
2021-10-17 15:44:29 +0500

    ERC20_Inhertance_contract(parent-child)

--- ERC721_ROBOTICs_NFT_MINTING/ERC721_Robotics_NFT_Minting/src/redux/store.js
+++ ERC721_ROBOTICs_NFT_MINTING/ERC721_Robotics_NFT_Minting/src/redux/store.js
@@ -2,6 +2,7 @@ import { applyMiddleware, compose, createStore, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import blockchainReducer from "./blockchain/blockchainReducer";
 import dataReducer from "./data/dataReducer";
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 const rootReducer = combineReducers({
   blockchain: blockchainReducer,
@@ -12,7 +13,7 @@ const middleware = [thunk];
 const composeEnhancers = compose(applyMiddleware(...middleware));
 
 const configureStore = () => {
-  return createStore(rootReducer, composeEnhancers);
+  return createStore(rootReducer,  composeWithDevTools(composeEnhancers)  );
 };
 
 const store = configureStore();
