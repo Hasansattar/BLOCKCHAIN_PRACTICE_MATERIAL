8c89581923a9aaff1f9172645b18d6024c96d79a
Hasansattar <hasansattar650@gmail.com>
2021-10-18 23:41:06 +0500

    SupplyChain-Dapp

---
+++ SupplyChain-Dapp/contracts/Item.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+import "./ItemManager.sol";
+
+contract Item {
+    uint public priceInWei;
+    uint public pricePaid;
+    uint public index;
+    
+    ItemManager parentContract;
+    
+    constructor(ItemManager _parentContract ,uint _priceInWei, uint _index) {
+        priceInWei = _priceInWei;
+        index = _index;
+        parentContract = _parentContract;
+    }
+    
+    receive() external payable {
+       
+    }
+    
+    fallback () external {
+    }
+}
