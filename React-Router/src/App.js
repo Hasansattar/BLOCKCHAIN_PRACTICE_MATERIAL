76cc0545bab2f56484709b02a5a7fe8d0e3ae983
Hasansattar <hasansattar650@gmail.com>
2021-10-22 03:36:35 +0500

    react router

---
+++ React-Router/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Page2 from './Page2';
+import Page3 from './Page3';
+
+
+function App() {
+  return (
+    <div>
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='page2' element={<Page2 />} />
+          <Route path='page3/:coolParam/*' element={<Page3 />} />
+        </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
