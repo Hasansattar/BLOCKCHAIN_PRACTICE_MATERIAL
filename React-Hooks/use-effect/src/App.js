a3882a13c439d1b0cf79fcf49277b1e81d9250f4
Hasansattar <hasansattar650@gmail.com>
2021-10-22 03:30:13 +0500

    React-Hooks

---
+++ React-Hooks/use-effect/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+import ClassMouse from './components/ClassMouse';
+import HookMouse from './components/HookMouse';
+import MouseContainer from './components/MouseContainer';
+import IntervalClassCounter from './components/IntervalClassCounter';
+import IntervalHookCounter from './components/IntervalHookCounter';
+import DataFetching from './components/DataFetching';
+import DataFetching2 from './components/DataFetching2';
+import DataFetching3 from './components/DataFetching3';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassCounter /> */}
+      {/* <HookCounter /> */}
+
+      {/* <ClassMouse /> */}
+      {/* <HookMouse /> */}
+      {/* <MouseContainer /> */}
+
+      {/* <IntervalClassCounter /> */}
+      {/* <IntervalHookCounter /> */}
+
+      {/* <DataFetching /> */}
+      {/* <DataFetching2/> */}
+      <DataFetching3/>
+    </div>
+  );
+}
+
+export default App;
